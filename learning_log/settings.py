541d9e23eabb005b1434b0652d2bb08d215d728b
Jinpen <kx180415@gmail.com>
2025-06-11 19:43:17 +0800

    Activating Models: use makemigrations and migrate, and modify learning_log/settings.py.
    Run python manage.py makemigrations learning_logs_app followed by python manage.py migrate. Also, add learning_logs_app to the INSTALLED_APPS list in learning_log/settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom apps
+    'learning_logs_app',
 ]
 
 MIDDLEWARE = [
